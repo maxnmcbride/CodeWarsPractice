04971d4b6f004e27a3c1314b5f1e3944b1f8b97c
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-04-20 11:54:12 -0400

    two sum

--- index.js
+++ index.js
@@ -154,6 +154,17 @@ function cockroachSpeed(s) {
   return Math.floor(cm)
 }
 
+// 23. Two Sum
+function twoSum(nums, target) {
+  const numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+    numMap.set(nums[i], i);
+  }
+}
 
 
 

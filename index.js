591a74cd90730d0bdf88dcabbb2fe88c4a707371
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-07-20 11:34:50 -0400

    fixed previous submission

--- index.js
+++ index.js
@@ -437,7 +437,7 @@ function peopleWithAgeDrink(old) {
     return 'drink toddy'
   } else if (old >= 14 && old < 18) {
     return 'drink coke'
-  } else if (old > 18 && old < 21) {
+  } else if (old >= 18 && old < 21) {
     return 'drink beer'
   } else {
     return 'drink whisky'
